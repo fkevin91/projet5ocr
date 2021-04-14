8f739975687c034a593e7e570ba5748fa3acf245
kevin fardeau <k.fardeau123@gmail.com>
2021-04-14 16:52:38 +0200

    List BlogPost

--- controller/ListPostController.php
+++ controller/ListPostController.php
@@ -1,6 +1,10 @@
 <?php
 
 use Twig\Environment;
+include '../vendor/autoload.php';
+// on inclus le modele
+include '../model/model.php';
+// On instancie une connexion
 
 class ListPostController {
 
@@ -11,8 +15,17 @@ class ListPostController {
     }
 
     public function displayListPost() {
-
-        echo $this->twig->render('listpost.html.twig', ['name' => 'Kévin']);
-
+        try {
+            $template = $this->twig->load('listpost.html.twig');
+            $affichageListPost = new connectionBDD();
+            $listpost = $affichageListPost->get_all_post();
+            $titre = "listPost";
+            echo $template->render(array(
+                'titre' => $titre,
+                'listpost' => $listpost,
+            ));
+        } catch (Exception $e) {
+            die ('ERROR: ' . $e->getMessage());
+        }
     }
 }
